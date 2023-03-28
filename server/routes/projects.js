c0f83c1836762b78a48d5282cdb3af9164845d0d
Sheraz <sherazp995@gmail.com>
2023-03-29 01:36:05 +0500

    Fixed some routes

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -3,12 +3,13 @@ var router = express.Router();
 var Project = require('../models/project');
 
 router.get('/', (req, res) => {
-  res.send({ Project: 'index'})
+  res.send({ Project: 'index' })
 });
 
 router.post("/create", async (req, res) => {
   try {
-    let result = await Project.findOne({ email: req.body.email });
+    let result = await Project.findOne({ email: req.body.title });
+    console.log(req.body)
     let message = '';
     message = 'Project Created Successfully!';
     result = await Project.create(req.body);
@@ -43,14 +44,24 @@ router.post('/update/:id', async (req, res) => {
 });
 
 router.get('/project_by_supervisor/:id', async (req, res) => {
-    try {
-      let result = await Project.find({ supervisorId: req.params.id });
-      res.status(200).json({ result, message: 'Success' });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: 'Somthing went wrong' });
-    }
-  });
+  try {
+    let result = await Project.find({ supervisorId: req.params.id });
+    res.status(200).json({ result, message: 'Success' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Somthing went wrong' });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    let result = await Project.find({ supervisorId: req.params.id });
+    res.status(200).json({ result, message: 'Success' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Somthing went wrong' });
+  }
+});
 
 router.get('/all', async (req, res) => {
   try {
